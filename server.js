d4af9cdd4dc9f6d1f10453c9fdb2efe34f9fae74
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-08-20 20:32:54 +0300

    fixed

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const { DB_HOST, PORT = 3000 } = process.env;
+main()
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3001");
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 
-app.listen(3001, () => {
-  console.log("Server running. Use our API on port: 3001")
-})
+async function main() {
+  await mongoose.connect(DB_HOST);
+}
