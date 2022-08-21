18e1682e48dfba04d45098a9035bfa8ed7bbe2d8
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-08-21 16:56:42 +0300

    Update updateFavorite.js

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -3,10 +3,12 @@ const { Contact } = require("../../schemas/contacts");
 const { RequestError } = require("../../helpers");
 
 const updateFavorite = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
-    throw RequestError(404, "Not found");
+    throw RequestError(400, "missing field favorite");
   }
   res.json(result);
 };
